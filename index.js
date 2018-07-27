504a5950eb978f3fc64080d804921e1946549eef
Bary Levi <barylevi@gmail.com>
2018-07-27 15:37:37 +0300

    Add prototype

---
+++ index.js
@@ -0,0 +1,25 @@
+rray.prototype.toString = function () {
+  return this.join('');
+};
+
+export default (state, selector, component) => {
+  const setState = setter => {
+    const execAfterRender = [];
+    const handlers = {};
+    const on = obj => {
+      let rand = Math.floor(Math.random() * 100000);
+      const identifier = `__miniact_events="${rand}"`;
+      for (let [event, handler] of Object.entries(obj)) {
+        execAfterRender.push(() => {
+          document.querySelector(`[${identifier}]`).addEventListener(event, handler);
+        });
+      }
+      return identifier;
+    }
+    state = Object.assign({}, state, setter(state));
+    document.querySelector(selector).innerHTML = component({state, setState, on});
+    execAfterRender.forEach(func => func());
+  }
+  setState(() => {});
+  return setState;
+};
